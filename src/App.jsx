1e2bb44854770e5cbfd437da76e77476a1e016c3
Omar Ochoa <omar@ochoawebdesign.com>
2023-01-18 16:44:14 -0800

    Creates the Post component'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import Post from "./components/Post";
+
 function App() {
 
-  return <h1>Hello World!</h1>
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Post />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post() {
+  return (
+    <div>
+      <p>Placeholder Name</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum, sit?</p>
+    </div>
+  );
+}
+
+export default Post; 
