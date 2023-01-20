2ec0b50dfa13ee80c257ebaa9beba811c517f11b
Omar Ochoa <omar@ochoawebdesign.com>
2023-01-19 17:30:51 -0800

    Uses usestate to update text in real time

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,12 +1,20 @@
+import {useState} from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
+  const [ enteredBodyText, setEnteredBodyText ] = useState('');
+
+  function changeBodyHandler(event) {
+    setEnteredBodyText(event.target.value);
+  }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
+      <p>{enteredBodyText}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
