de40ced6d07c5b3da826ad1bd465570b1a845b2d
Omar Ochoa <omar@ochoawebdesign.com>
2023-01-19 16:32:42 -0800

    Component Post: Creates and implements Module CSS

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,10 @@
-function Post(props) {
+import classes from './Post.module.css';
 
+function Post(props) {
   return (
-    <div>
-      <h2>{props.author}</h2>
-      <p>{props.body}</p>
+    <div className={classes.post}>
+      <h2 className={classes.author}>{props.author}</h2>
+      <p className={classes.text}>{props.body}</p>
     </div>
   );
 }
