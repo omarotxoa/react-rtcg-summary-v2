707cac725e77b8ae567f86ef4f6c65ffe7b6a7f9
Omar Ochoa <omar@ochoawebdesign.com>
2023-01-19 16:53:04 -0800

    Creates NewPost component in preperation for state management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import NewPost from "./NewPost";
 
 function PostsList() {
   return(
-    <ul className={classes.posts}>
-      <Post author="Rin" body="React.js is awesome!"/>
-      <Post author="Phynn" body="Lorem ipsum sit dolor" />
-      <Post author="Ryan" body="Lorem test" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Rin" body="React.js is awesome!"/>
+        <Post author="Phynn" body="Lorem ipsum sit dolor" />
+        <Post author="Ryan" body="Lorem test" />
+      </ul>
+    </>
   );
 }
 
