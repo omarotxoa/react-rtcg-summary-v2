258edd69453b21cb949b087c4bc1c2f5023563f3
Omar Ochoa <omar@ochoawebdesign.com>
2023-02-02 18:57:19 -0800

    Adds dynamic info update into the posts

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,23 +1,16 @@
 import {useState} from 'react';
 import classes from './NewPost.module.css';
 
-function NewPost() {
-  const [ enteredBodyText, setEnteredBodyText ] = useState('');
-
-  function changeBodyHandler(event) {
-    setEnteredBodyText(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
       </p>
-      <p>{enteredBodyText}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange}/>
       </p>
     </form>
   );
